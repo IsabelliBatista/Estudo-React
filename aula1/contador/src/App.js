af45c9850d351f3b3ea66fb43d4827ef459640fa
Isabelli Batista <isabellibatista7@gmail.com>
2020-04-06 20:21:14 -0300

    aula virtual dh react

---
+++ aula1/contador/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Contador from './componentes/Contador';
+
+
+const App = () => {
+  return (
+    <Contador />
+  );
+}
+
+export default App;
---
+++ aula1/contador/src/componentes/Contador.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import './Contador.css';
+
+const Contador = () => {
+
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="container">
+            <h1>Contador de ReactJs</h1>
+            <span>{count}</span>
+            <div className="botoes">
+                <button onClick={() => setCount(count + 1)}>Incrementar</button>
+                <button onClick={() => setCount(count - 1)}>Decrementar</button>
+            </div>
+        </div>
+    )
+}
+export default Contador;
